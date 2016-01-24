1c078adbd3876db8f9cc310af0177e1db4f06fbc
alanbfgit <alanbf@gmail.com>
2016-01-23 19:43:45 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-consumer_key = 'your-consumer-key'
-consumer_secret = 'your-consumer-secret'
-access_token = 'your-access-token'
-access_secret = 'your-access-secret'
+access_token = "16226678-frJAvZX1hkcOIrBAKs7Vmem5vx4XDYUdVXzABRCQC"
+access_token_secret = "f8Zh0eyuJB49RJi5aO5EB1uYLpaOIxU3GQ4ULULGh7A5H"
+consumer_key = "uTqsqrmUqckfSDyBvK4H9K8m2"
+consumer_secret = "ZFE9PVxsE1wmPEJvrqKjRpvLAutMhejfXL49ny2fiNqsiEgSrs"
